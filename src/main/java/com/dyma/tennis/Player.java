16e4173c251b1833a791ab823b62a28da45b53b1
bchaises <benjamin.chaises@gmail.com>
2025-07-27 15:19:55 +0200

    Add web layer

---
+++ src/main/java/com/dyma/tennis/Player.java
@@ -0,0 +1,17 @@
+package com.dyma.tennis;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public record Player(
+        @NotBlank(message = "First name is mandatory") String firstName,
+        @NotBlank(message = "Last name is mandatory") String lastName,
+        @NotNull(message = "Birth date is mandatory") @PastOrPresent(message = "Birth date must be past or present") LocalDate birthDate,
+        @Valid Rank rank
+) {
+}
