ca4ef75eaa0edf37c42ebe96e80e1ddc97cefffc
bchaises <benjamin.chaises@gmail.com>
2025-08-05 19:45:35 +0200

    Add header security and authorizations

--- src/main/java/com/dyma/tennis/security/SecurityConfiguration.java
+++ src/main/java/com/dyma/tennis/security/SecurityConfiguration.java
@@ -3,7 +3,7 @@ package com.dyma.tennis.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -39,12 +39,27 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.
                 csrf(csrf -> csrf.disable())
+                .headers(header ->
+                        header
+                                .contentSecurityPolicy(csp ->
+                                    csp.policyDirectives("default src 'self' data:;style-src 'self' 'unsafe-inline';")
+                                )
+                                .frameOptions(frameOptions -> frameOptions.deny())
+                                .permissionsPolicyHeader(permissionsPolicyConfig ->
+                                    permissionsPolicyConfig.policy("fullscreen=(self), geolocation=(), microphone=(), camera=()")
+                                )
+                )
                 .authorizeHttpRequests(authorization -> authorization
-                .requestMatchers("/swagger-ui/**").permitAll()
-                .requestMatchers("/v3/api-docs/**").permitAll()
-                .requestMatchers("/accounts/login").permitAll()
-                .anyRequest().authenticated()
-            );
+                        .requestMatchers(HttpMethod.GET, "/players/**").hasAuthority("ROLE_USER")
+                        .requestMatchers(HttpMethod.POST, "/players/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/players/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/players/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/healthcheck/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/accounts/login").permitAll()
+                        .anyRequest().authenticated()
+                );
         return http.build();
     }
 }
